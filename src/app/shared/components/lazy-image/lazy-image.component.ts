10b069e5bd61a2ad3d8ef1412b6e0d8474434af7
Giselle <ABASLEALGIS@GMAIL.COM>
2024-02-16 11:39:54 -0600

    commit final

--- src/app/shared/components/lazy-image/lazy-image.component.ts
+++ src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -1,7 +1,27 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'shared-lazy-image',
   templateUrl: './lazy-image.component.html'
 })
-export class LazyImageComponent { }
+export class LazyImageComponent implements OnInit {
+
+  @Input()
+  public url!:string;
+
+  @Input()
+  public alt:string = '';
+
+  public hasLoaded:boolean=false;
+
+  ngOnInit(): void {
+  if (!this.url) throw new Error ('URL propperty is required');
+  }
+  onLoad(){
+    setTimeout(() => {
+      this.hasLoaded= true;
+    }, 1000);
+
+
+  }
+}
